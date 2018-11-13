25215d7e4743e0ef7392bc6ebe3ab52ce0923c19
root123 <root123@dbmslab9.(none)>
2018-11-13 10:02:44 +0530

    Added some files

--- bell.java
+++ bell.java
@@ -1,85 +1,85 @@
 import java.util.Scanner;
-public class BellmanFord
+public class bell
 {
-private int D[];
-private int num_ver;
-public static final int MAX_VALUE = 999;
-public BellmanFord(int n)
-{
-this.n=n;
-D = new int[n+1];
-}
-public void shortest(int s,int A[][])
-{
-for (int i=1;i<=n;i++)
-{
-D[i]=MAX_VALUE;
-}
-D[s] = 0;
-for(int k=1;k<=n-1;k++)
-{
-for(int i=1;i<=n;i++)
-{
-for(int j=1;j<=n;j++)
-{
-if(A[i][j]!=MAX_VALUE)
-{
-if(D[j]>D[i]+A[i][j])
-D[j]=D[i]+A[i][j];
-}
-}
-}
-}
-for(int i=1;i<=n;i++)
-{
-for(int j=1;j<=n;j++)
-{
-if(A[i][j]!=MAX_VALUE)
-{
-if(D[j]>D[i]+A[i][j])
-{
-System.out.println("The Graph contains negative egde cycle");
-return;
-}
-}
-}
-}
-for(int i=1;i<=n;i++)
-{
-System.out.println("Distance of source " + s + " to "+ i + " is " + D[i]);
-}
-}
-public static void main(String[ ] args)
-{
-int n=0,s;
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the number of vertices");
-n = sc.nextInt();
+	private int D[];
+	private int num_ver;
+	public static final int MAX_VALUE = 999;
+	private int n;
+	public bell(int n)
+	{
+		this.n=n;
+		D = new int[n+1];
+	}
+	public void shortest(int s,int A[][])
+	{
+		for (int i=1;i<=n;i++)
+		{
+			D[i]=MAX_VALUE;
+		}
+		D[s] = 0;
+		for(int k=1;k<=n-1;k++)
+		{
+			for(int i=1;i<=n;i++)
+			{
+				for(int j=1;j<=n;j++)
+				{
+					if(A[i][j]!=MAX_VALUE)
+					{
+						if(D[j]>D[i]+A[i][j])
+							D[j]=D[i]+A[i][j];
+					}
+				}
+			}
+		}
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(A[i][j]!=MAX_VALUE)
+				{
+					if(D[j]>D[i]+A[i][j])
+					{
+						System.out.println("The Graph contains negative egde cycle");
+						return;
+					}
+				}
+			}
+		}
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println("Distance of source " + s + " to "+ i + " is " + D[i]);
+		}
+	}
+	public static void main(String[ ] args)
+	{
+		int n=0,s;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of vertices");
+		n = sc.nextInt();
 
-2017-18
-int A[][] = new int[n+1][n+1];
-System.out.println("Enter the Weighted matrix");
-for(int i=1;i<=n;i++)
-{
-for(int j=1;j<=n;j++)
-{
-A[i][j]=sc.nextInt();
-if(i==j)
-{
-A[i][j]=0;
-continue;
-}
-if(A[i][j]==0)
-{
-A[i][j]=MAX_VALUE;
-}
-}
-}
-System.out.println("Enter the source vertex");
-s=sc.nextInt();
-BellmanFord b = new BellmanFord(n);
-b.shortest(s,A);
-sc.close();
-}
+		int A[][] = new int[n+1][n+1];
+		System.out.println("Enter the Weighted matrix");
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				A[i][j]=sc.nextInt();
+				if(i==j)
+				{
+					A[i][j]=0;
+					continue;
+				}
+				if(A[i][j]==0)
+				{
+					A[i][j]=MAX_VALUE;
+				}
+			}
+		}
+		System.out.println("Enter the source vertex");
+		s=sc.nextInt();
+		bell b = new bell(n);
+		b.shortest(s,A);
+		sc.close();
+	}
 }
 
